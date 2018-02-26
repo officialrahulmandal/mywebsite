d86ee321bce40364a3b274a87453df4c02bf6276
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-02-26 22:27:17 +0530

    created my first template

--- articles/views.py
+++ articles/views.py
@@ -4,15 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album
+from django.template import loader
 def index(request):
     all_albums=Album.objects.all()
-    html=' '
-    for album in all_albums:
-        url='/article/'+str(album.id)+'/'
-        html+='<a href="' + url+'">' + album.album_title + '</a><br>'
-        return HttpResponse(html)
+    template=loader.get_template('articles/index.html')
+    context={
+    'all_albums':all_albums,}
+    
+    return HttpResponse(template.render(context,request))
 
-    return HttpResponse("<h1>hello welcome to article section")
 def detail(request,album_id):
     return HttpResponse("<h2> Deatils for album id :-"+str(album_id)+"</h2>")
 
