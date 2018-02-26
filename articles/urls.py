465246397b6c052ead444ac4f77ae6b9761e43cb
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-02-26 16:53:31 +0530

    creating another view

--- articles/urls.py
+++ articles/urls.py
@@ -1,3 +1,5 @@
 from django.conf.urls import url
 from . import views
-urlpatterns = [url(r'^$',views.index,name='index'),]
+urlpatterns = [url(r'^$',views.index,name='index'),
+url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
+]
--- articles/views.py
+++ articles/views.py
@@ -5,5 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>hello welcome to article section")
+def detail(request,album_id):
+    return HttpResponse("<h2> Deatils for album id :-"+str(album_id)+"</h2>")
 
 # Create your views here.
