4ba1e52eaa1419faa5739ec3bb49e835bd0f3504
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-02-26 16:29:16 +0530

    first model is created and added to admin interface

--- articles/admin.py
+++ articles/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Album,song
 # Register your models here.
+admin.site.register(Album)
+admin.site.register(song)
