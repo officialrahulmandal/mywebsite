fa0e66fb3bc5739ad018ffc664b398990a14d791
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-02-26 22:38:35 +0530

    HttpResponse replaced with render

--- articles/views.py
+++ articles/views.py
@@ -7,11 +7,8 @@ from .models import Album
 from django.template import loader
 def index(request):
     all_albums=Album.objects.all()
-    template=loader.get_template('articles/index.html')
-    context={
-    'all_albums':all_albums,}
-    
-    return HttpResponse(template.render(context,request))
+    return render(request,'articles/index.html',{'all_albums':all_albums,})
+
 
 def detail(request,album_id):
     return HttpResponse("<h2> Deatils for album id :-"+str(album_id)+"</h2>")
